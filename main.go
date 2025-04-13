3575b68612083b37799c71264e3ddc55e1ef714a
Amandeep2230 <bhalla.aman99@gmail.com>
2025-04-12 20:49:46 -0400

    feat: ball & paddle behavior

--- main.go
+++ main.go
@@ -9,16 +9,57 @@ var brick_height int16 = 20
 var brick_offset_x int16 = 2
 var brick_offset_y int16 = 2
 var window_width int16 = 622
-var window_height int16 = 950
+var window_height int16 = 800
 
 var paddle_width int16 = 80
 var paddle_height int16 = 30
 var paddle_x int16 = (window_width - paddle_width) / 2
 var paddle_y int16 = window_height - (paddle_height + 10)
+var paddle_vel int16 = 4
 
 var ball_size float32 = 10.0
 var ball_x int16 = window_width / 2
 var ball_y int16 = window_height - (paddle_height + 45)
+var ball_vel int16 = 4
+
+func ball_spawn() {
+	var ball_bottom int16 = ball_y + int16(ball_size)
+
+	//ball behaviour
+	if ball_bottom >= window_height {
+		ball_vel = 0
+		ball_y = window_height - int16(ball_size)
+	}
+
+	ball_y += ball_vel
+	ball_vel += 1
+
+	rl.DrawCircle(int32(ball_x), int32(ball_y), ball_size, rl.Red)
+}
+
+func paddle_spawn() {
+	var paddle_right int16 = paddle_x + paddle_width
+
+	rl.DrawRectangle(int32(paddle_x), int32(paddle_y), int32(paddle_width), int32(paddle_height), rl.Black)
+
+	//paddle behavior
+	if rl.IsKeyDown(rl.KeyLeft) {
+		paddle_x -= paddle_vel
+	}
+
+	if rl.IsKeyDown(rl.KeyRight) {
+		paddle_x += paddle_vel
+	}
+
+	if paddle_x <= 0 {
+		paddle_x = 2
+	}
+
+	if paddle_right >= window_width {
+		paddle_x = window_width - (paddle_width + 2)
+	}
+
+}
 
 func set_scene() {
 	rl.ClearBackground(rl.RayWhite)
@@ -29,9 +70,8 @@ func set_scene() {
 		}
 	}
 
-	rl.DrawRectangle(int32(paddle_x), int32(paddle_y), int32(paddle_width), int32(paddle_height), rl.Black)
-
-	rl.DrawCircle(int32(ball_x), int32(ball_y), ball_size, rl.Red)
+	paddle_spawn()
+	ball_spawn()
 }
 
 func main() {
@@ -46,5 +86,4 @@ func main() {
 		rl.EndDrawing()
 	}
 
-	//testing push from other device
 }
